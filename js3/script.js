40a4f8971e8686e0719ea6c300724d94c8876dbd
giosaginashvili <164656337+giosaginashvili@users.noreply.github.com>
2024-06-12 22:36:44 -0700

    Add files via upload

---
+++ js3/script.js
@@ -0,0 +1,68 @@
+let num =10
+switch (num) {
+    case 10:
+        console.log(10)
+        break;
+        case 10:
+        console.log(10)
+        break;
+        
+        case 11:
+        console.log(11)
+        break;
+        
+        case 12:
+        console.log(12)
+        break;
+
+        case 13:
+        console.log(13)
+        break;
+
+        case 14:
+            console.log(14)
+            break;
+
+           default:
+            console.log(15)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+let ne =["lasha","maka","giorgi","nini","mari"]
+
+ne.shift()
+ne.push("lasha")
+
+ if (ne [4]== "lasha") {
+    console.log ("lasha")
+}
+
+else if (ne [4]=="maka") {
+    console.log("maka")
+}
+
+else if (ne [4]=="giorgi") {
+    console.log("giorgi")
+}
+
+else if (ne [4]=="nini"){
+    console.log("nini")
+}
+   
+
+else {
+    console.log("mari")
+}
